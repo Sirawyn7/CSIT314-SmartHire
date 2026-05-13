f64dc38de812358f8adff526a86defbef3f1b63b
Sirawyn7 <steven.trivett1@gmail.com>
2026-05-13 12:48:12 +1000

    Setup Base HTML Page
    - Setup base.html to insert footer and header into centralised location for other pages to build off
    - Did Initial CSS to get design visually appealing
    - Setup /base url for testing of UI

--- routes/auth.py
+++ routes/auth.py
@@ -10,6 +10,7 @@ class AuthRoutes:
         self._register_routes()
 
     def _register_routes(self):
+        self.blueprint.add_url_rule("/base", view_func=self.base, methods=["GET"])
         self.blueprint.add_url_rule("/ping/auth", view_func=self.ping, methods=["GET"])
         self.blueprint.add_url_rule("/login", view_func=self.login_page, methods=["GET"])
         self.blueprint.add_url_rule("/register", view_func=self.register_page, methods=["GET"])
@@ -18,6 +19,9 @@ class AuthRoutes:
         self.blueprint.add_url_rule("/api/auth/register", view_func=self.register, methods=["POST"])
         self.blueprint.add_url_rule("/api/auth/forgot-password", view_func=self.forgot_password_submit, methods=["POST"])
         self.blueprint.add_url_rule("/api/auth/logout", view_func=self.logout, methods=["POST"])
+
+    def base(self):
+        return render_template("base.html")
 
     def ping(self):
         return jsonify({"message": "Auth routes are working"}), 200
